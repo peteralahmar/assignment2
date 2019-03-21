0845d90bda36548d1f4188211450b2a203b44189
peteralahmar <peteralahmar@cmail.carleton.ca>
2019-03-21 18:22:43 -0400

    final version

--- ass2_q1.m
+++ ass2_q1.m
@@ -6,51 +6,64 @@
 %solving question 1 part a
 x=40;
 y=60;
-
-%matrix G and V
-G = sparse((x *y), (x*y));
-V = zeros(1,(x *y));
 Vo = 1;
+%matrix G and V
+G = sparse((x * y), (x * y));
+V = zeros(1,(x *y));
 
 for i = 1:x
     
     for j = 1: y
         n = j+ (i-1) *y;
         
-        if i == x
+        if (i == 1)
             G(n,:) = 0;
+            
             G(n,n) = 1;
             
-        elseif i == 1 
-            G(n,:) = 0;
-            G(n,n) = 1;
             V(1,n) = 1;
             
-        elseif i>1 && i <x && j==1
+        elseif (i == x)
+            G(n,:) = 0;
+            
+            G(n,n) = 1;
+
+            
+        elseif ((i > 1) && (i < x) && (j==1))
+            
             G(n, n+y) = 1;
+            
             G(n,n) = -3;
+            
             G(n,n-y) = 1;
+            
             G(n,n-1) = 1;
-        elseif j ==y && i < x && i >1
+            
+        elseif ((j == y) && (i < x) && (i >1))
             G(n, n+y) = 1;
+            
             G(n,n) = -3;
+            
             G(n,n-y) = 1;
-            G(n,n-1) = 1;
+            
+            G(n,n+1) = 1;
         else
             G(n, n+y) = 1;
+            
             G(n,n) = -4;
+            
             G(n,n-y) = 1;
+            
             G(n,n-1) = 1;
+            
             G(n,n+1) = 1;
         end
     end
 end
-figure(1)
-spy(G)
+figure(1);
+spy(G);
+
 solution = G\V';
-
-figure(2)
-
 %matrix
 act = zeros(x,y);
 
@@ -60,61 +73,75 @@ for i =1:x
         act(i,j) = solution(n);
     end
 end
-
+figure(2);
 surf(act);
 
 
 %solving question 1 part b
 
-newG = sparse((x *y),(x*y));
-newV = zeros(1,(x *y));
-%same V0
+x=40;
+y=60;
+Vo = 1;
+
+newG = sparse((x * y),(x * y));
+newV = zeros(1,(x * y));
 
 for i = 1:x
     
-    for j = 1: y
+    for j = 1:y
+        
         n = j+ (i-1) *y;
         
-        if i == x
-            newV(1,n) =1; % which is Vo
+        if (i == 1)
+
             newG(n,:) =0;
+            
             newG(n,n) =1;
             
-        elseif i == 1 
-            newV(1,n) =1; % which is Vo
-            newG(n,:) =0;
-            newG(n,n) =1;
+            newV(1,n) =1; % which is Vo 
+            
+        elseif (i == x) 
 
-        elseif j ==y
-            newG(n,n) = 1;
+            newG(n,:) =0;
+            
+            newG(n,n) =1;
+           
+            newV(1,n) = 1;
+        elseif (i == y)                      
             newG(n,:) = 0;
             
-        elseif j==1
             newG(n,n) = 1;
-            newG(n,:) = 0
+            
+        elseif (j == 1)        
+            newG(n,:) = 0;
+            
+            newG(n,n) = 1;
 
         else
-            newG(n, n+y) = 1;
-            newG(n,n) = -4;
-            newG(n,n-y) = 1;
-            newG(n,n-1) = 1;
-            newG(n,n+1) = 1;
             newG(n,:) = 0;
+            
+            newG(n,n) = -4;
+            
+            newG(n,n-y) = 1;
+            
+            newG(n,n+y) = 1;
+            
+            newG(n,n-1) = 1;
+            
+            newG(n,n+1) = 1;
+            
+
         end
     end
 end
 
-figure(3)
-spy(newG)
-
 newSolution = newG\newV';
-
-figure(4)
-
+figure(3);
 newAct = zeros(x,y);
 
 for i = 1:x
     for j = 1:y
+        
         n = j + (i-1)*y;
         newAct(i,j) = newSolution(n);
        
@@ -123,20 +150,26 @@ end
 
 surf(newAct)
 
-%using analytical approach -- reached out for help -- isnt working -- 
+%using analytical approach -- reached out for help -- 
 
-a = 60; b= 20;
-z = zeros(60,40);
-numx = linspace(-20,20,40);
-numy = linspace(0,60,60);
+z = zeros(60, 40);
+a = 60;b = 20;
 
-[xm,ym] = meshgrid(numx, numy);
+xnum = linspace(-20,20,40);
+ynum = linspace(0,60,60);
 
-for i = 1: 2:300
-    z = (4* Vo/pi) .* (z + (cosh((n*pi*xm)/a)./ (n* cosh((n*pi*b)/a)) .* sin((n*pi*ym)/a))) ;
-    figure(5)
-    title("Analytical Approach")
-    surf(numx,numy,z)
-    pause(0.01)
+[xm, ym] = meshgrid(xnum, ynum);
+
+
+
+for n = 1:2:300
+    
+    z= (4 * Vo/pi) .* (z + (cosh((n * pi * xm)/a) .* sin((n * pi * ym)/a)) ./ (n * cosh((n * pi * b)/a)));
+    
+    figure(4);
+    surf(xnum, ynum, z);
+    title("Analytical Approach");
+    pause(0.01);
+    
 end
 
--- ass2_q2.m
+++ ass2_q2.m
@@ -10,7 +10,7 @@ Vo = 1;
 conductivity1 = 1; %outside
 conductivity2 = 1e-2; %inside
 
-box1 = [(x* 2/5), (x* 3/5),y , (y *4/5)]; %lower box
+box1 = [(x* 2/5), (x* 3/5), y , (y *4/5)]; %lower box
 box2 = [(x* 2/5), (x* 3/5),0, (y *1/5)]; %top box
 
 bottleNeckVec = zeros(1,10);
@@ -27,9 +27,9 @@ for i = 1:x
     end
 end
 %plotting the map
-figure(1)
-title('Conductivity Map')
-surf(map)
+figure(1);
+surf(map);
+title('Conductivity Map');
 
 
 %Matrix G
@@ -39,40 +39,36 @@ for i = 1:x
         
         n = j+(i-1) *y;
         
-        
         if i==x
-            G(n,n) = 1;
             G(n,:) = 0;
+            G(n,n) = 1;
 
         elseif i==1
-            V(1,n) = 1; %Vo 
-            G(n,n) = 1;
             G(n,:) = 0;  
-            
+            G(n,n) = 1;
+            V(1,n) = 1; %Vo 
         elseif j ==1 && i>1 && i<x
+            
             if i == box1(1)
-                
+                G(n,n) = -3;
+                G(n,n+1) = conductivity2;
                 G(n,n+y) = conductivity2;
-                G(n,n) = -3;
-                G(n,n-y) = conductivity1;
-                G(n,n+1) = conductivity2;  
+                G(n,n-y) = conductivity1;  
             elseif i ==box1(2)
-                
+                G(n,n) = -3;
+                G(n,n+1) = conductivity2;
                 G(n,n+y) = conductivity1;
-                G(n,n) = -3;
-                G(n,n-y) = conductivity2;
-                G(n,n+1) = conductivity2;
+                G(n,n-y) = conductivity2;  
             elseif (i >box1(1) && i< box1(2))
-                
-                G(n, n+y) = conductivity1;
                 G(n,n) = -3;
-                G(n,n-y) = conductivity2;
                 G(n,n+1) = conductivity2;
+                G(n,n+y) = conductivity2;
+                G(n,n-y) = conductivity2;  
             else
-                G(n, n+y) = conductivity1;
                 G(n,n) = -3;
-                G(n,n-y) = conductivity1;
                 G(n,n+1) = conductivity1;
+                G(n,n+y) = conductivity1;
+                G(n,n-y) = conductivity1;  
                 
             end
             
@@ -81,59 +77,55 @@ for i = 1:x
         elseif j ==y && i >1 && i <x
             
             if i == box1(1)
-                G(n, n+y) = conductivity2;
                 G(n,n) = -3;
+                G(n, n-1) = conductivity2;
+                G(n,n+y) = conductivity2;
                 G(n,n-y) = conductivity1;
-                G(n,n-1) = conductivity2;
             elseif i == box1(2)
-                G(n, n+y) = conductivity1;
                 G(n,n) = -3;
+                G(n, n-1) = conductivity2;
+                G(n,n+y) = conductivity1;
                 G(n,n-y) = conductivity2;
-                G(n,n-1) = conductivity2;
             elseif(i >box1(1)&& i <box1(2))
-                G(n, n+y) = conductivity2;
                 G(n,n) = -3;
+                G(n, n-1) = conductivity2;
+                G(n,n+y) = conductivity2;
                 G(n,n-y) = conductivity2;
-                G(n,n-1) = conductivity2;
             else
-                G(n, n+y) = conductivity1;
                 G(n,n) = -3;
+                G(n, n-1) = conductivity1;
+                G(n,n+y) = conductivity1;
                 G(n,n-y) = conductivity1;
-                G(n,n-1) = conductivity1;
             end
         else
             if i == box1(1) && (j < box2(4) || (j > box1(4)))
-                G(n,n+1) = conductivity2;  
-                G(n, n+y) = conductivity2;
                 G(n,n) = -4;
-                G(n,n-y) = conductivity1;
+                G(n,n+1) = conductivity2;  
                 G(n,n-1) = conductivity2;
+                G(n, n+y) = conductivity2;
+                G(n,n-y) = conductivity1;
             elseif i == box1(2) && (j < box2(4) || (j > box1(4)))
-                G(n,n+1) = conductivity2;  
-                G(n, n+y) = conductivity1;
                 G(n,n) = -4;
-                G(n,n-y) = conductivity2;
+                G(n,n+1) = conductivity2;  
                 G(n,n-1) = conductivity2;
+                G(n, n+y) = conductivity1;
+                G(n,n-y) = conductivity2;
             elseif i > box1(1) && i <box1(2) && (j < box2(4) || (j > box1(4)))
+                G(n,n) = -4;
                 G(n,n+1) = conductivity2;  
-                G(n, n+y) = conductivity2;
-                G(n,n) = -4;
-                G(n,n-y) = conductivity2;
                 G(n,n-1) = conductivity2;
-            else 
-                G(n,n+1) = conductivity1;  
-                G(n, n+y) = conductivity1;
-                G(n,n) = -4;
+                G(n, n+y) = conductivity2;
                 G(n,n-y) = conductivity1;
+            else 
+                G(n,n) = -4;
+                G(n,n+1) = conductivity1;  
                 G(n,n-1) = conductivity1;
+                G(n, n+y) = conductivity1;
+                G(n,n-y) = conductivity1;
             end      
         end
     end 
 end
-figure(2)
-title('G matrix')
-spy(G)
-
 
 solution1 = G\V';
 
@@ -147,54 +139,42 @@ for i =1:x
 end
 
 
-figure(3)
-title('Map with Bottleneck')
-surf(act)
+figure(2);
+surf(act);
+title('Voltage Map with Bottleneck');
 
 [Efx,Efy] = gradient(act);
 Efx = -Efx;
 Efy = -Efy;
 
-figure(4)
-title('Quiver Plot of Electric Field')
+figure(3)
 axis tight
 quiver(Efx',Efy');
+title('Quiver Plot of Electric Field')
 
 %plotting current density
 
 Jx = Efx .* map;
 Jy = Efx .*map;
-figure(5)
-title('Quiver Plot of Current Density (J)')
+figure(4)
 axis tight
-quiver(Jx,Jy)
+quiver(Jx,Jy);
+title('Quiver Plot of Current Density (J)')
 
 magnitude = ((Jx .^2) + (Jy .^2)) .^0.5;
 
-figure(6)
+figure(5)
 title('Magnitude of Current Density')
 surface(magnitude)
 axis tight
 
 currentflow = sum(Jx(x/2,:));
 
-figure(7)
+figure(6)
 title('Electric Field along the Y direction')
 surface(Efy)
 
-figure(8)
+figure(7)
 title('Electric Field along the X direction')
 surface(Efx)
-
-
-
-
-
-
-
-
-
-
-
-
 
